48e27249a60d45c79243574873cfd5f3a30c2dc9
Bifei-Hao <146667921+Bifei-Hao@users.noreply.github.com>
2024-09-23 16:27:12 -0500

    Add files via upload

--- basic_potion.py
+++ basic_potion.py
@@ -1,5 +1,5 @@
 import argparse
-
+import torch
 class SimpleOptions():
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Simple Options for Training')
@@ -9,6 +9,7 @@ class SimpleOptions():
         self.parser.add_argument('--batch_size', type=int, default=4, help='input batch size')
         self.parser.add_argument('--initial_lr', type=float, default=1e-4, help='initial learning rate')
         self.parser.add_argument('--num_epochs', type=int, default=36, help='number of epochs to train')
+        self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for cpu')
         self.parser.add_argument('--senarios', type=str, nargs='+', default=['ball_00', 'ball_02', 'basket_05', 'eggs_01', 'eggs_03', 'eggs_05', 'horse_03', 'may29_handheld_02', 'may29_handheld_04'], help='list of scenarios')
         self.parser.add_argument('--data_root_dir', type=str, default='/home/nwn9209/tsfnet/tsfnet_code/EXP1_dataset', help='root directory for data')
 
@@ -18,8 +19,12 @@ class SimpleOptions():
         if not self.initialized:
             self.initialize()
         self.opt = self.parser.parse_args()
+        str_ids = self.opt.gpu_ids.split(',')
+        self.opt.gpu_ids = str_ids[0]
+        
         args = vars(self.opt)
-
+        
+        
         print('------------ Options -------------')
         for k, v in sorted(args.items()):
             print(f'{k}: {v}')
