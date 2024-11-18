7122f466a7114d942ea670b13255384c2c6519c2
Lucas WU <87957430+gegebenheiten@users.noreply.github.com>
2024-11-18 16:12:44 -0600

    Add files via upload

--- training.py
+++ training.py
@@ -3,9 +3,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.multiprocessing as mp
-import torch.distributed as dist
 from torch.utils.data import DataLoader
-from torch.utils.data.distributed import DistributedSampler
 from tqdm import tqdm
 from models.EDSR import EDSR
 from dataset import HSERGBDataset
@@ -16,7 +14,6 @@ from basic_option import SimpleOptions
 import pickle
 import time
 from datetime import datetime
-from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.tensorboard import SummaryWriter  
 
 def mse(labels, output):
@@ -31,11 +28,6 @@ def psnr(labels, output, max_value=255.0):
         return float('inf')  
     return 10 * torch.log10(max_value ** 2 / mse_value)
 
-def init_process(rank, size, fn, backend='nccl'):
-    """Initialize the distributed environment."""
-    dist.init_process_group(backend, rank=rank, world_size=size)
-    fn(rank, size)
-
 def validate(opt, model, val_loader, writer, epoch):
     model.eval()  
     step = 0
@@ -47,24 +39,22 @@ def validate(opt, model, val_loader, writer, epoch):
             for i, (events_forward, events_backward, left_image, right_image, gt_image, weight, [n_left, n_right],
                     surface, left_voxel_grid, right_voxel_grid, name) in enumerate(loader):
 
-                    events_forward = events_forward.cuda()
-                    events_backward = events_backward.cuda()
-                    left_image = left_image.cuda()
-                    right_image = right_image.cuda()
-                    gt_image = gt_image.cuda()
+                    # events_forward = events_forward.to(opt.device)
+                    # events_backward = events_backward.to(opt.device)
+                    left_image = left_image.to(opt.device)
+                    right_image = right_image.to(opt.device)
+                    gt_image = gt_image.to(opt.device)
                     # weight = weight.cuda()
                     # surface = surface.cuda()
-                    left_voxel_grid = left_voxel_grid.cuda()
-                    right_voxel_grid = right_voxel_grid.cuda()
+                    left_voxel_grid = left_voxel_grid.to(opt.device)
+                    right_voxel_grid = right_voxel_grid.to(opt.device)
 
                     # Forward pass
                     output, residue = model(opt, left_image, left_voxel_grid, right_image, right_voxel_grid)
 
                     # Calculate loss
-                    output_denorm = (output + 1) / 2 * 255
-                    labels_denorm = (gt_image + 1) / 2 * 255
-                    L1_loss = nn.L1Loss()(output_denorm, labels_denorm)
-                    ssim_error = ssim( labels_denorm, output_denorm, data_range=255, size_average=True)
+                    L1_loss = nn.L1Loss()(output, gt_image)
+                    ssim_error = ssim( gt_image, output, data_range=1.0, size_average=True)
                     loss = L1_loss * 0.15 + (1 - ssim_error)  * 0.85
                     val_loss += loss.item()
                     ssim_sum += ssim_error
@@ -87,17 +77,13 @@ def validate(opt, model, val_loader, writer, epoch):
     print(f'Validation Loss: {avg_val_loss:.4f}, Average L1_loss:{avg_L1_loss:.4f}, Average SSIM: {avg_ssim:.4f}')
     print('--------Validation-----------')
 
-    # Clear GPU memory after validation
-    torch.cuda.empty_cache()
 
-
-def train(rank, size):
-    # Set the device for each process
-    torch.cuda.set_device(rank)  # Ensure each process uses a different GPU
-    opt = SimpleOptions().parse()
-    opt.device = torch.device('cuda', rank)  # Set device for current process
+def main():
+    option = SimpleOptions()
+    opt = option.parse()
     opt.isTrain = True
     opt.isValidate = True
+    opt.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     n_blocks = (opt.image_height // opt.block_size) * (opt.image_width // opt.block_size)
     senarios = [f.name for f in os.scandir(os.path.join(opt.data_root_dir, 'train')) if f.is_dir()]
@@ -106,15 +92,13 @@ def train(rank, size):
     val_senarios = [f.name for f in os.scandir(os.path.join(opt.data_root_dir, 'val')) if f.is_dir()]
     opt.val_senarios = val_senarios
 
-    # Prepare training data with DistributedSampler
+    # Prepare training data
     train_dataset = [HSERGBDataset(opt.data_root_dir, 'train', k, opt.skip_number, opt.nmb_bins) for k in senarios]
-    train_sampler = DistributedSampler(train_dataset[rank], num_replicas=size, rank=rank)
-    train_loader = DataLoader(train_dataset[rank], batch_size=opt.batch_size, sampler=train_sampler, num_workers=8)
-
+    train_loader = [torch.utils.data.DataLoader(train_dataset[k], batch_size=opt.batch_size, shuffle=False, pin_memory=False, num_workers=1) for k in range(len(train_dataset))]
     # Prepare validation data
     if opt.isValidate == True:
         val_dataset = [HSERGBDataset(opt.data_root_dir, 'val', k, opt.skip_number, opt.nmb_bins) for k in val_senarios]
-        val_loader = [torch.utils.data.DataLoader(val_dataset[k], batch_size=1, shuffle=False, pin_memory=False, num_workers=8) for k in range(len(val_dataset))]
+        val_loader = [torch.utils.data.DataLoader(val_dataset[k], batch_size=1, shuffle=False, pin_memory=False, num_workers=1) for k in range(len(val_dataset))]
     
    
     with open(f'data_stats/div2k/stats_qp{opt.qp}.pkl', 'rb') as f:
@@ -122,11 +106,11 @@ def train(rank, size):
 
     dct_min = torch.from_numpy(stats['dct_input']['min'][None, :, None, None]).float().to(opt.device)
     dct_max = torch.from_numpy(stats['dct_input']['max'][None, :, None, None]).float().to(opt.device)
-
+    # pdb.set_trace()
     model = EDSR(n_blocks, dct_max, dct_min).to(opt.device)
-    model = DDP(model, device_ids=[rank])
-
+    # criterion = nn.L1Loss()
     optimizer = optim.Adam(model.parameters(), lr=opt.initial_lr)
+
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=12, gamma=0.1)
 
     num_params = sum(p.numel() for p in model.parameters())
@@ -134,6 +118,10 @@ def train(rank, size):
 
     log_dir = f'./logs/{datetime.now().strftime("%Y%m%d_%H%M%S")}'
     writer = SummaryWriter(log_dir=log_dir)
+
+    # if torch.cuda.device_count() > 1:
+    #     print("Using", torch.cuda.device_count(), "GPUs for training!")
+    #     model = nn.DataParallel(model)
 
     step = 0
     for epoch in range(opt.num_epochs):
@@ -145,38 +133,33 @@ def train(rank, size):
         mae_sum = 0.0
         ssim_sum = 0.0
         psnr_sum = 0.0
-
-        train_sampler.set_epoch(epoch)  # Shuffle data at the start of each epoch
-        
         for loader in train_loader:
             for i, (events_forward, events_backward, left_image, right_image, gt_image, weight, [n_left, n_right],
                     surface, left_voxel_grid, right_voxel_grid, name) in enumerate(loader):
-                events_forward = events_forward.cuda()
-                events_backward = events_backward.cuda()
-                left_image = left_image.cuda()
-                right_image = right_image.cuda()
-                gt_image = gt_image.cuda()
+                # events_forward = events_forward.to(opt.device)
+                # events_backward = events_backward.to(opt.device)
+                left_image = left_image.to(opt.device)
+                right_image = right_image.to(opt.device)
+                gt_image = gt_image.to(opt.device)
                 # weight = weight.cuda()
                 # surface = surface.cuda()
-                left_voxel_grid = left_voxel_grid.cuda()
-                right_voxel_grid = right_voxel_grid.cuda()
+                left_voxel_grid = left_voxel_grid.to(opt.device)
+                right_voxel_grid = right_voxel_grid.to(opt.device)
                 batch_start_time = time.time()
 
                 optimizer.zero_grad()
                 output, residue = model(opt, left_image, left_voxel_grid, right_image, right_voxel_grid)
 
-                output_denorm = (output + 1) / 2  * 255
-                labels_denorm = (gt_image + 1) / 2 * 255
-                L1_loss = nn.L1Loss()(output_denorm, labels_denorm)
-                ssim_error = ssim(labels_denorm, output_denorm, data_range=255, size_average=True)
+                L1_loss = nn.L1Loss()(output, gt_image)
+                ssim_error = ssim(gt_image, output, data_range=1.0, size_average=True)
                 loss = L1_loss * 0.15 + (1 - ssim_error) * 0.85
                 
                 loss.backward()
                 optimizer.step()
 
-                mean_square_error = mse(labels_denorm, output_denorm)
-                mean_absolute_error = mae(labels_denorm, output_denorm)
-                psnr_error = psnr(labels_denorm, output_denorm)
+                mean_square_error = mse(gt_image, output)
+                mean_absolute_error = mae(gt_image, output)
+                psnr_error = psnr(gt_image, output)
 
                 train_loss += loss.item()
                 L1_loss_sum += L1_loss.item()
@@ -190,8 +173,6 @@ def train(rank, size):
                 print(f"[Epoch {epoch + 1}, Batch {i + 1}] Loss: {loss:.3f}, L1_loss:{L1_loss:.3f}, ssim:{ssim_error:.3f}, Batch Time: {batch_time:.3f} sec")
 
                 step+=1
-                            
-            torch.cuda.empty_cache()
 
         # writer.add_images('Model Output', output_denorm, epoch)
         # writer.add_images('Ground Truth', labels_denorm, epoch)
@@ -238,10 +219,6 @@ def train(rank, size):
     print(f"Model saved to {model_save_path}")
 
     writer.close()
-
-def main():
-    size = torch.cuda.device_count()  # Number of GPUs
-    mp.spawn(init_process, args=(size, train), nprocs=size, join=True)
 
 if __name__ == '__main__':
     main()
