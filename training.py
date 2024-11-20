daae8ffd3bf6384c9ed4da4875612b1434cfcf8d
Lucas WU <87957430+gegebenheiten@users.noreply.github.com>
2024-11-20 10:54:24 -0600

    Add files via upload

--- training.py
+++ training.py
@@ -22,7 +22,7 @@ def mse(labels, output):
 def mae(labels, output):
     return torch.mean(torch.abs(labels - output))
 
-def psnr(labels, output, max_value=255.0):
+def psnr(labels, output, max_value=1.0):
     mse_value = mse(labels, output)
     if mse_value == 0:
         return float('inf')  
@@ -36,31 +36,25 @@ def validate(opt, model, val_loader, writer, epoch):
     L1_loss_sum = 0.0
     with torch.no_grad():  
         for loader in val_loader:
-            for i, (events_forward, events_backward, left_image, right_image, gt_image, weight, [n_left, n_right],
-                    surface, left_voxel_grid, right_voxel_grid, name) in enumerate(loader):
+            for i, (left_image, right_image, gt_image, left_voxel_grid, right_voxel_grid, name) in enumerate(loader):
+                left_image = left_image.to(opt.device)
+                right_image = right_image.to(opt.device)
+                gt_image = gt_image.to(opt.device)
+                left_voxel_grid = left_voxel_grid.to(opt.device)
+                right_voxel_grid = right_voxel_grid.to(opt.device)
 
-                    # events_forward = events_forward.to(opt.device)
-                    # events_backward = events_backward.to(opt.device)
-                    left_image = left_image.to(opt.device)
-                    right_image = right_image.to(opt.device)
-                    gt_image = gt_image.to(opt.device)
-                    # weight = weight.cuda()
-                    # surface = surface.cuda()
-                    left_voxel_grid = left_voxel_grid.to(opt.device)
-                    right_voxel_grid = right_voxel_grid.to(opt.device)
+                # Forward pass
+                output, _ = model(opt, left_image, left_voxel_grid, right_image, right_voxel_grid)
 
-                    # Forward pass
-                    output, residue = model(opt, left_image, left_voxel_grid, right_image, right_voxel_grid)
-
-                    # Calculate loss
-                    L1_loss = nn.L1Loss()(output, gt_image)
-                    ssim_error = ssim( gt_image, output, data_range=1.0, size_average=True)
-                    loss = L1_loss * 0.15 + (1 - ssim_error)  * 0.85
-                    val_loss += loss.item()
-                    ssim_sum += ssim_error
-                    L1_loss_sum += L1_loss.item()
-                    
-                    step+=1
+                # Calculate loss
+                L1_loss = nn.L1Loss()(output, gt_image)
+                ssim_error = ssim( gt_image, output, data_range=1.0, size_average=True)
+                loss = L1_loss * 0.15 + (1 - ssim_error)  * 0.85
+                val_loss += loss.item()
+                ssim_sum += ssim_error.item()
+                L1_loss_sum += L1_loss.item()
+                
+                step+=1
                     
     # Average validation loss and SSIM
     avg_val_loss = val_loss / step
@@ -94,7 +88,7 @@ def main():
 
     # Prepare training data
     train_dataset = [HSERGBDataset(opt.data_root_dir, 'train', k, opt.skip_number, opt.nmb_bins) for k in senarios]
-    train_loader = [DataLoader(train_dataset[k], batch_size=opt.batch_size, shuffle=False, pin_memory=False, num_workers=0) for k in range(len(train_dataset))]
+    train_loader = [DataLoader(train_dataset[k], batch_size=opt.batch_size, shuffle=True, pin_memory=False, num_workers=0) for k in range(len(train_dataset))]
     # Prepare validation data
     if opt.isValidate == True:
         val_dataset = [HSERGBDataset(opt.data_root_dir, 'val', k, opt.skip_number, opt.nmb_bins) for k in val_senarios]
@@ -134,21 +128,16 @@ def main():
         ssim_sum = 0.0
         psnr_sum = 0.0
         for loader in train_loader:
-            for i, (events_forward, events_backward, left_image, right_image, gt_image, weight, [n_left, n_right],
-                    surface, left_voxel_grid, right_voxel_grid, name) in enumerate(loader):
-                # events_forward = events_forward.to(opt.device)
-                # events_backward = events_backward.to(opt.device)
+            for i, (left_image, right_image, gt_image, left_voxel_grid, right_voxel_grid, name) in enumerate(loader):
                 left_image = left_image.to(opt.device)
                 right_image = right_image.to(opt.device)
                 gt_image = gt_image.to(opt.device)
-                # weight = weight.cuda()
-                # surface = surface.cuda()
                 left_voxel_grid = left_voxel_grid.to(opt.device)
                 right_voxel_grid = right_voxel_grid.to(opt.device)
                 batch_start_time = time.time()
 
                 optimizer.zero_grad()
-                output, residual = model(opt, left_image, left_voxel_grid, right_image, right_voxel_grid)
+                output, _ = model(opt, left_image, left_voxel_grid, right_image, right_voxel_grid)
 
                 L1_loss = nn.L1Loss()(output, gt_image)
                 ssim_error = ssim(gt_image, output, data_range=1.0, size_average=True)
@@ -163,16 +152,20 @@ def main():
 
                 train_loss += loss.item()
                 L1_loss_sum += L1_loss.item()
-                mse_sum += mean_square_error
-                mae_sum += mean_absolute_error
-                ssim_sum += ssim_error
-                psnr_sum += psnr_error
+                mse_sum += mean_square_error.item()
+                mae_sum += mean_absolute_error.item()
+                ssim_sum += ssim_error.item()
+                psnr_sum += psnr_error.item()
 
                 # Log the loss and metrics to TensorBoard
                 batch_time = time.time() - batch_start_time
-                print(f"[Epoch {epoch + 1}, Batch {i + 1}] Loss: {loss:.3f}, L1_loss:{L1_loss:.3f}, ssim:{ssim_error:.3f}, Batch Time: {batch_time:.3f} sec")
+                print(f"[Epoch {epoch + 1}, Senario:{name[0].split('/')[-3]}, Batch {i + 1}] Loss: {loss.item():.3f}, L1_loss:{L1_loss.item():.3f}, ssim:{ssim_error.item():.3f}, Batch Time: {batch_time:.3f} sec")
 
                 step+=1
+
+            # torch.cuda.empty_cache()
+            print(f"Allocated memory: {torch.cuda.memory_allocated() / 1024 ** 2:.2f} MB")
+            print(f"Reserved memory: {torch.cuda.memory_reserved() / 1024 ** 2:.2f} MB")
 
         # writer.add_images('Model Output', output_denorm, epoch)
         # writer.add_images('Ground Truth', labels_denorm, epoch)
