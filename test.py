ccb6cee1a17b9b0038fe29de9c197278700382a8
Lucas WU <87957430+gegebenheiten@users.noreply.github.com>
2024-11-21 12:20:06 -0600

    Add files via upload

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from models.EDSR import EDSR
-from dataset import demoDataset
+from dataset import HSERGBDataset
 import numpy  as np
 import os
 from basic_option import SimpleOptions
-from util import save_output_as_png, denormalize_output, images_to_video, ssim, psnr, mse, mae
+from pytorch_msssim import ssim
+from util import save_output_as_png, denormalize_output, images_to_video, psnr, mse, mae
 import pickle
 import time
 import math
@@ -63,16 +64,16 @@ opt = option.parse()
 opt.isTrain = False
 opt.isValidate = False
 opt.isTest = True
-opt.device = torch.device(opt.gpu_ids)
+opt.device = torch.device('cpu')
 # senarios = [f.name for f in os.scandir(os.path.join(opt.data_root_dir,'1_TEST')) if f.is_dir()]
 # opt.senarios = senarios
-opt.senarios = ['ball_05']
+opt.senarios = ['water_bomb_floor_01']
 
-n_blocks = (opt.image_height // opt.block_size) * (opt.image_width // opt.block_size)
+n_blocks = (640 // opt.block_size) * (1024 // opt.block_size)
 new_size = (opt.image_height, opt.image_width)
 # Prepare data
-test_dataset = demoDataset(opt)
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+test_dataset = [HSERGBDataset(opt.data_root_dir, 'test', k, opt.skip_number, opt.nmb_bins) for k in opt.senarios]
+test_loader = [DataLoader(test_dataset[k], batch_size=opt.batch_size, shuffle=True, pin_memory=False, num_workers=0) for k in range(len(test_dataset))]
 
 with open(f'data_stats/div2k/stats_qp{opt.qp}.pkl', 'rb') as f:
     stats = pickle.load(f)
@@ -82,7 +83,7 @@ dct_max = torch.from_numpy(stats['dct_input']['max'][None, :, None, None]).float
 # Initialize model, criterion, and optimizer
 model = EDSR(n_blocks, dct_max, dct_min).to(opt.device)
 
-model_path = 'save_models/model_epoch_100_batch_16_insert_1.pth'
+model_path = 'save_models/model_epoch_40_batch_32_insert_5_small.pth'
 state_dict = torch.load(model_path)
 new_state_dict = {}
 for key in state_dict:
@@ -98,21 +99,23 @@ result_mae = []
 result_psnr = []
 result_ssim = []
 
-with torch.no_grad():  # 不计算梯度
-    for se_idx in range(len(opt.senarios)):
+with torch.no_grad():  
+    for loader in test_loader:
         idx = 0
-        for i, (inputs, labels)  in enumerate(test_loader):
-            opt.se_idx = se_idx  # 假设输入数据键为 'input'
-            left_image, right_image, left_events, right_events = inputs
-            dir =os.path.join('./output', opt.senarios[opt.se_idx])
-            if not os.path.exists(dir):
-                os.makedirs(dir,exist_ok=True)
-            output = model(opt, left_image, left_events, right_image, right_events) # 生成预测
+        for i, (left_image, right_image, gt_image, left_voxel_grid, right_voxel_grid, name) in enumerate(loader):
+            # Forward pass
+            left_image = left_image.to(opt.device)
+            right_image = right_image.to(opt.device)
+            left_voxel_grid = left_voxel_grid.to(opt.device)
+            right_voxel_grid = right_voxel_grid.to(opt.device)
+
+            output, _ = model(opt, left_image, left_voxel_grid, right_image, right_voxel_grid)
+
             output = denormalize_output(output=output.squeeze(0).cpu().numpy())
-            labels = denormalize_output(labels.squeeze(0).cpu().numpy())
+            labels = denormalize_output(gt_image.squeeze(0).cpu().numpy())
             result_mse.append(mse(labels, output))
             result_mae.append(mae(labels, output))
-            result_ssim.append(ssim(labels, output))
+            result_ssim.append(ssim(labels, output, data_range=255.0, size_average=True))
             result_psnr.append(psnr(labels, output))
             # output = np.array([cv2.resize(output[j, :, :], new_size) for j in range(output.shape[0])])
             output = np.transpose(output, (1, 2, 0))  # 转换维度为 (H, W, 3)
@@ -135,17 +138,17 @@ with torch.no_grad():  # 不计算梯度
     print(np.mean(result_mse), np.mean(result_mae), np.mean(result_psnr), np.mean(result_ssim)) 
 
 
-image_folder = "output/ball_05"  # 替换为你的 PNG 文件夹路径
-output_video = f"./ball_05_large_insert_{opt.skip_number}.mp4"  # 输出 MP4 文件名
+image_folder = "output/water_bomb_floor_01"  # 替换为你的 PNG 文件夹路径
+output_video = f"./water_bomb_floor_01_{opt.skip_number}.mp4"  # 输出 MP4 文件名
 fps = 30 # 设置帧率
 
 images_to_video(image_folder, output_video, fps = 30)
 
-# input_folder = "data/bs_ergb/1_TEST/ball_05/images"  
-# input_video = "./input_ball_05.mp4"  
-# fps = 30 # 设置帧率
+input_folder = "data/hsergb/test/water_bomb_floor_01/images_corrected"  
+input_video = "./input_water_bomb_floor_01.mp4"  
+fps = 30 # 设置帧率
 
-# images_to_video(input_folder, input_video, 1, fps)
+images_to_video(input_folder, input_video, 1, fps)
 
-# GT_video = "./GT_ball_05.mp4"  
-# images_to_video(input_folder, GT_video)
+GT_video = "./GT_water_bomb_floor_01.mp4"  
+images_to_video(input_folder, GT_video)
